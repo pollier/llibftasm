c0e0cf6ac45a654ef2d6a479cc83553ed1c68d94
Your Name <you@example.com>
2015-06-01 19:16:41 +0200

    gtcp

--- ft_puts.s
+++ ft_puts.s
@@ -6,14 +6,14 @@
 ;    By: pollier <pollier@student.42.fr>            +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2015/06/01 18:15:52 by pollier           #+#    #+#              ;
-;    Updated: 2015/06/01 18:18:18 by pollier          ###   ########.fr        ;
+;    Updated: 2015/06/01 19:16:32 by pollier          ###   ########.fr        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
-global _ft_strcat
+global _ft_puts
 section .text
 
-_ft_strcat:
+_ft_puts:
 
 	push rdi
 
